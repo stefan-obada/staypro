890bdb0098a75686e9aae79c6e2101f7f32ae5cb
stefan-obada <stefan.obada@yahoo.com>
2020-09-08 18:43:00 +0100

    added support for Timer module

--- app/main.py
+++ app/main.py
@@ -21,60 +21,51 @@ from kivy.uix.label import Label
 from kivy.lang.builder import Builder
 from kivy.core.window import Window
 from kivy.clock import Clock
+from kivy.logger import Logger
 
-from kivy.properties import ObjectProperty, StringProperty
+from timer import get_timer
+
+UPDATE_INTERVAL = 0.1
 
 
 class BaseLayout(ABC):
     """ Protocol for all layouts in the app. """
 
+    def exit(self, screen, *args, **kwargs):
+        Window.unbind(on_key_down=self.keyact)
+        self.manager.get_screen(screen).entry(*args, **kwargs)
+        self.manager.current = screen
+
+        self.exit_add(*args)
+
+    def entry(self, *args, **kwargs):
+        Window.bind(on_key_down=self.keyact)
+
+        self.entry_add(*args, **kwargs)
+
     @abstractmethod
-    def exit_point(self, screen):
-        """ Should unbind everything from keyboard and do """
+    def keyact(self, *args):
+        """ This should capture every key """
         return NotImplementedError
 
-    @abstractmethod
-    def _entry_point(self):
-        """ Should change the screen and bind keyboard inputs """
-
-
-class MainLayout(Screen):
-
-    def entry_point(self):
-        Window.bind(on_key_down=self.keyact)
-
-    def exit_point(self, screen):
+    def entry_add(self, *args, **kwargs):
+        """ What else do you want when entering """
         pass
 
-    def keyact(self, *args):
-        key_code = args[1]
-        if key_code == 13:  # Enter key
-            print("Starting activity...")
-            self.start_activity()
-
-    def start_activity(self):
-        # Set RUNTIME text to activity and start timer
-        runtime_screen = self.manager.get_screen("runtime")
-        runtime_screen.start_timer()
-        runtime_screen.update_activity(activity=self.ids.main_activity.text)
-
-        # Set screen
-        self.manager.current = "runtime"
+    def exit_add(self, *args, **kwargs):
+        """ What else do you want when exiting """
+        pass
 
 
-class LoginLayout(Screen):
+class MetaMerge(type(Screen), type(BaseLayout)):
+    pass
 
-    def entry_point(self):
-        Window.bind(on_key_down=self.keyact)
 
-    def exit_point(self, screen):
-        Window.unbind(on_key_down=self.keyact)
-        self.manager.get_screen(screen).entry_point()
-        self.manager.current = screen
+class LoginLayout(BaseLayout, Screen, metaclass=MetaMerge):
 
     def __init__(self, **kwargs):
         super(Screen, self).__init__(**kwargs)
-        self.entry_point()
+        self.entry_add()
 
     def keyact(self, *args):
         key_code = args[1]
@@ -87,59 +78,74 @@ class LoginLayout(Screen):
         pwd = self.ids.login_pwd.text
         if user is "a" and pwd is "b":
             # TODO change this to Database
-            self.exit_point("main")
+            self.exit(screen="main")
         else:
             print(self)
             self.add_widget(Label(text="Invalid", color=(1, 0, 0, 1)))
 
 
+class MainLayout(BaseLayout, Screen, metaclass=MetaMerge):
 
-class RuntimeLayout(Screen):
+    def keyact(self, *args):
+        key_code = args[1]
+        if key_code == 13:  # Enter key
+            print("Starting activity...")
+            self.start_activity()
 
-    def entry_point(self):
+    def start_activity(self):
+        self.exit(screen="runtime", activity=self.ids.main_activity.text)
+
+
+class RuntimeLayout(BaseLayout, Screen, metaclass=MetaMerge):
+
+    def keyact(self, *args):
         pass
 
-    def exit_point(self, screen):
-        pass
+    def entry_add(self, *args, **kwargs):
+        # Set activity
+        self.activity = kwargs.get("activity", "None")
+        self.ids.runtime_current_activity.text = self.activity
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # self.bind()
+        # Start timer
+        self.timer = get_timer(activity=self.activity, update_interval=UPDATE_INTERVAL/3)
+        self.timer.start()
         self.paused = False
 
-    def keydown(self, *args):
-        print("null")
+        # Make sure button is on PAUSED
+        self.ids.runtime_pause_btn.text = "PAUSED"
+
+        # Schedule time update on screen
+        Clock.schedule_interval(self.update_time, self.timer.update_interval/2)
+
+    def exit_add(self, *args, **kwargs):
+        self.timer.stop()
+
 
     def stop(self):
-        self.stop_timer()
-        self.manager.current = "main"
+        self.exit(screen="main")
 
-    def update_time(self, start_time, *args):
-        eta = datetime.timedelta(seconds=(int(time.time() - start_time)))
-        processed_eta = eta.__str__()
-        self.ids.runtime_current_time.text = processed_eta
-
-    def update_activity(self, activity):
-        self.ids.runtime_current_activity.text = activity
-
-    def start_timer(self):
-        start_time = time.time()
-        self.timer = Clock.schedule_interval(partial(self.update_time, start_time), 1)
-
-    def stop_timer(self):
-        if self.timer:
-            self.timer.cancel()
-
-    def pause_timer(self):
-        if self.paused:
-            self.start_timer()
-            self.ids.runtime_pause_btn.text = "PAUSE"
-            self.paused = False
-        else:
-            start_pause_time = time.time()
-            self.stop_timer()
-            self.ids.runtime_pause_btn.text = "RESUME"
+    def pause(self):
+        if not self.paused:
+            # Pause timer
+            self.timer.pause()
             self.paused = True
+
+            # Switch button from "PAUSE" to "RESUME"
+            self.ids.runtime_pause_btn.text = "RESUME"
+
+            return
+
+        if self.paused:
+            # Re-start the time
+            self.timer.start()
+
+            # Switch button from "RESUME" to "PAUSE"
+            self.ids.runtime_pause_btn.text = "PAUSE"
+
+            return
+
+    def update_time(self, *args):
+        self.ids.runtime_current_time.text = self.timer.get_hhmmss()
 
 
 class MainApp(App):
@@ -158,126 +164,3 @@ class MainApp(App):
 
 if __name__ == '__main__':
     MainApp().run()
-
-# # Get screen size
-# w_screen, h_screen = sg.Window.get_screen_size()
-# sg.theme('TanBlue')
-# # #
-
-# # Log file
-# if not path.isdir('SP_log'):
-#     mkdir('SP_log')
-# log_path = 'SP_log/' + 'log.csv'
-#
-# if not path.isfile(log_path):  # First usage per day
-#     with open(log_path, 'w') as first_file:
-#         first_file.write('Date,Activity,Minutes,Seconds\n')
-#         # Next row is for future implementation
-#         # first_file.write('Activity,Start,End,Minutes,Seconds\n')
-#
-# # #
-#
-# # Build 1st window
-# layout1 = [
-#     [sg.InputText(default_text='', key='-ACTIVITY-', focus=True, size=(26, 1))],
-#     [sg.Button(button_text='GO', key='go', bind_return_key=True), sg.Button(button_text='EXIT', key='exit')]
-# ]
-# window1 = sg.Window(title='StayPro', layout=layout1, location=(0, h_screen - 150), use_ttk_buttons=True,
-#                     alpha_channel=0.85, grab_anywhere=True, element_justification='center', keep_on_top=True)
-#
-#
-# # #
-#
-#
-# def current_time(start_time, lost_time):
-#     """ Time accounting for pauses """
-#     return int(round(time.time() * 100)) - start_time - int(round(lost_time * 100))
-#
-#
-# def update_log(activity, start_time, lost_time):
-#     # Next 2 rows are for future implementation
-#     # end_ascii_time = time.asctime().split()[3]
-#     # start_ascii_time = time.ctime(start_time).split()[3]
-#
-#     try:
-#         with open(log_path, 'a') as log_file:
-#             ct = current_time(start_time, lost_time)
-#             log_file.write("{},{},{:02d},{:02d}\n".format(date.today().isoformat(),
-#                                                           activity, (ct // 100) // 60, (ct // 100) % 60))
-#     except PermissionError as e:
-#         sg.Popup('Please close the LOG file.', title='ERROR', )
-#         update_log(activity, start_time, lost_time)
-#
-#
-# def main():
-#     while True:
-#         event, values = window1.read()
-#
-#         if event in (None, 'exit'):  # Sys close or Exit
-#             window1.close()
-#             break
-#
-#         elif event == 'go':  # Press GO, change to window2
-#
-#             # Get the previous input and hide window1
-#             activity = values['-ACTIVITY-']
-#             window1.hide()
-#             # #
-#
-#             # Build 2nd window
-#             layout2 = [
-#                 [sg.Text(activity, key='-CURRENT_ACTIVITY-'), sg.Text('Time:', key='-TIME-')],
-#                 [sg.Button(button_text='PAUSE', key='pause', auto_size_button=True),
-#                  sg.Button(button_text='CANCEL', key='cancel')]
-#             ]
-#             window2 = sg.Window(title='StayPro', layout=layout2, location=(0, h_screen - 150), use_ttk_buttons=True,
-#                                 alpha_channel=0.85, grab_anywhere=True, element_justification='center',
-#                                 keep_on_top=True,
-#                                 return_keyboard_events=True)
-#             # #
-#
-#             start_time = int(round(time.time() * 100))
-#             lost_time = 0  # used for counting time in pauses
-#             window2_finished = False
-#
-#             while not window2_finished:  # Window 2 loop
-#                 event, values = window2.read(timeout=10)
-#
-#                 if event in (None, 'cancel'):  # If cancel, return to window1, update log
-#                     update_log(activity, start_time, lost_time)
-#
-#                     # window2_finished = True # Useless
-#                     window2.close()
-#                     window1.un_hide()
-#                     break
-#
-#                 elif event == 'pause':  # PAUSE loop (if PAUSE/Space are pressed)
-#                     start_pause_time = time.time()
-#
-#                     window2['pause'].update('RESUME')
-#
-#                     while True:  # PAUSE Loop until RESUME/Space is pressed
-#                         event, values = window2.read()
-#
-#                         if event in (None, 'cancel'):  # Go back to window1, update log
-#
-#                             # Update log in 'a' mode
-#                             update_log(activity, start_time, lost_time)
-#
-#                             window2_finished = True
-#                             window2.close()
-#                             window1.un_hide()
-#                             break
-#
-#                         elif event == 'pause':  # If RESUME or Space is pressed, stay chill
-#                             lost_time = lost_time + time.time() - start_pause_time
-#                             window2['pause'].update('PAUSE')
-#                             break
-#
-#                 ct = current_time(start_time, lost_time)
-#                 window2['-TIME-'].update('{:02d}:{:02d}'.format((ct // 100) // 60,
-#                                                                 (ct // 100) % 60))
-#
-#         else:  # Any other button is pressed, do nothing
-#             pass
-#
