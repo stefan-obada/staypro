8e9b0d2339bbf7aa5c9766bb94c6fcef67652fa5
stefan-obada <stefan.obada@yahoo.com>
2020-09-08 18:42:34 +0100

    solved logical backend

--- app/timer.py
+++ app/timer.py
@@ -3,8 +3,9 @@ from kivy.logger import Logger
 from multiprocessing import Process
 import time
 
-def get_timer():
-    timer = Timer()
+
+def get_timer(activity, update_interval=0.5):
+    timer = Timer(activity=activity, update_interval=update_interval)
     timer.load()
     return timer
 
@@ -15,35 +16,44 @@ class Timer:
     Load with load()
     Destroy with stop()
     """
+
     def __init__(self, activity="", update_interval=0.5):
         self.update_interval = update_interval
         self.activity = activity
         self.__time = 0
-        self.paused = False
 
     def log_info(self, message: str):
         Logger.info(f"{self.activity}: {message}")
 
     def load(self):
-        self.thread = threading.Timer(interval=self.update_interval, function=self.update_time)
+        self.thread = threading.Thread(target=self.update_time)
         self.log_info("Timer loaded.")
 
     def update_time(self):
-        self.__time += self.update_interval
+        while True:
+            if self.paused:
+                return
+
+            self.__time += self.update_interval
+            time.sleep(self.update_interval)
 
     def start(self):
+        if not self.thread.is_alive():
+            self.load()
+        self.paused = False
         self.thread.start()
         self.log_info("Timer started.")
-        self.paused = False
+
 
     def stop(self):
-        self.thread.cancel()
+        self.paused = True
+        self.thread.join()
         self.log_info("Timer stopped.")
 
     def pause(self):
-        self.stop()
-        self.log_info("Timer paused.")
         self.paused = True
+        self.thread.join()
+        self.log_info("Timer paused.")
 
     @property
     def time(self):
@@ -57,13 +67,19 @@ class Timer:
             self.__time = 0
 
     def get_hhmmss(self):
-        secs = self.__time
+        secs = int(self.__time)
         mins, secs = divmod(secs, 60)
         hours, mins = divmod(mins, 60)
 
         if mins == 0:
-            return f"{secs}"
+            if secs < 10:
+                return f"00:0{secs}"
+            else:
+                return f"{secs}"
         elif hours == 0:
-            return f"{mins}:{secs}"
+            if mins < 10:
+                return f"0{mins}:{secs}"
+            else:
+                return f"{mins}:{secs}"
         else:
             return f"{hours}:{mins}:{secs}"
